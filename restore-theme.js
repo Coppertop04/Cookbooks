137b391759dd5bd2a7b27e2564f013216a25b25c
Coppertop04 <dylanmayres@gmail.com>
2026-03-09 18:18:48 -0400

    a

--- restore-theme.js
+++
@@ -1,8 +0,0 @@
-(function() {
-  const savedTheme = localStorage.getItem('user-theme');
-  if (savedTheme) {
-    document.body.classList.add(savedTheme);
-  } else {
-    document.body.classList.add('lux'); // Your default
-  }
-})();
